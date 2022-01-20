bc31a8c7ea0d744c30e497378af59aa51e1d8dfc
9 <9@DESKTOP-22AU890>
2022-01-20 14:40:31 +0900

    DAY8

---
+++ DAY8/src/test1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class test1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int MAX = 5;
+		
+		int[] Id = new int[MAX];
+		int[] Pw = new int[MAX];
+		int[] Money = new int[MAX];
+		
+		int count = 0;
+		int log = -1;
+					
+		System.out.println("ID를 입력해주세요.");
+		scanner.nextInt();
+		if(int id != Id) {
+			System.out.println("");
+		}
+		else( == Id) {
+			System.out.println("중복된 ID입니다.");
+			break;
+		}
+		
+		System.out.println("메뉴를 선택해주세요.");
+		int selset = 0;
+		if(select = [1]) {
+			System.out.println("입금할 금액을 선택해주세요.");
+		}
+		else if(select = [2]){
+			System.out.println("이체할 금액을 선택해주세요.");
+		}
+		else if(select = [3]) {
+			System.out.println("잔액조회를 시작합니다.");
+		}
+		else { //나가기
+	 System.out.println("");
+			
+	}
+	}
+}
